cf9a907acfe1d6dc90fadfa87a128d5e3e4a1946
Reza Gerami <r.gerami25@gmail.com>
2021-01-19 08:53:06 +0330

    A little modify the codes

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Header/>
   <div class="container mt-5">
     <div class="col-md-8 offset-md-2">
       <Calendar/>
@@ -8,8 +9,10 @@
 
 <script>
 import Calendar from '@/components/Calender'
+import Header from '@/components/Header'
 export default {
   components:{
+    Header,
     Calendar,
   },
 };
